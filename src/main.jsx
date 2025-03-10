d13eed8507b3e470163fad41f9d17e4534f92938
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2025-03-10 16:39:55 +0530

    Rename Register.jsx to Step1.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Root from "/src/routes/Root.jsx";
 import "./index.css";
-import Register from "./routes/Register";
+import Step1 from "./routes/Step1";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <Routes>
         <Route path="/" element={<Root />}>
-          <Route index element={<Register />} />
+          <Route index element={<Step1 />} />
         </Route>
       </Routes>
     </StrictMode>
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Navbar from '../components/Navbar.jsx'
+import Navbar from "../components/Navbar.jsx";
 import { Outlet } from "react-router";
 
 export default function Root() {
   return (
     <div className="flex flex-wrap justify-center content-center">
-        <div className="main w-screen h-screen bg-primary-Light-blue">
-            
+      <div className="main w-screen h-screen bg-primary-Light-blue">
         <Navbar />
         <Outlet />
-        </div>
+      </div>
     </div>
   );
 }
