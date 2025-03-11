6900e19b571aa299d3062f0b7b0ff5185c3e5fd5
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2025-03-11 12:45:48 +0530

    design step3 page

--- src/routes/Step2.jsx
+++ src/routes/Step2.jsx
@@ -47,91 +47,109 @@ export default function Step2() {
   return (
     <div className="relative h-4/5 w-full lg:h-full">
       <div className="absolute top-[-8%] left-[50%] flex h-auto w-[92%] translate-x-[-50%] flex-col gap-4 rounded-lg bg-neutral-white p-8 drop-shadow-xl md:w-[600px] lg:static lg:w-full lg:translate-x-0 lg:gap-6 lg:p-16 lg:drop-shadow-none">
-      <h1 className="text-2xl lg:text-4xl font-bold text-primary-marine-blue">
+        <h1 className="text-2xl font-bold text-primary-marine-blue lg:text-4xl">
           Select your plan
         </h1>
         <h2 className="text-neutral-cool-gray lg:text-xl">
           You have the option of monthly or yearly billing.
         </h2>
         <div className="flex flex-col gap-2">
-          {!isOn?monthlyPlan.map((item, index) => {
-            const isChecked = item.plan === currentPlan;
-            return (
-              <div
-                className={`flex flex-wrap content-center rounded-lg border-2 p-4 hover:border-primary-purplish-blue ${isChecked ? "border-primary-purplish-blue bg-primary-purplish-blue/5" : "border-neutral-light-gray bg-neutral-white"}`}
-                key={index}
-              >
-                <input
-                  type="radio"
-                  className="hidden"
-                  name="plan"
-                  id={item.plan}
-                  value={item.plan}
-                  onChange={(e) => setcurrentPlan(e.target.value)}
-                />
-                <label className=" w-full flex gap-4" htmlFor={item.plan}>
-                  <div className="planicon">
-                    <img src={planslogo[index]} alt="logo" />
+          {!isOn
+            ? monthlyPlan.map((item, index) => {
+                const isChecked = item.plan === currentPlan;
+                return (
+                  <div
+                    className={`flex flex-wrap content-center rounded-lg border-2 p-4 hover:border-primary-purplish-blue ${isChecked ? "border-primary-purplish-blue bg-primary-purplish-blue/5" : "border-neutral-light-gray bg-neutral-white"}`}
+                    key={index}
+                  >
+                    <input
+                      type="radio"
+                      className="hidden"
+                      name="plan"
+                      id={item.plan}
+                      value={item.plan}
+                      onChange={(e) => setcurrentPlan(e.target.value)}
+                    />
+                    <label className="flex w-full gap-4" htmlFor={item.plan}>
+                      <div className="planicon">
+                        <img src={planslogo[index]} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-medium text-primary-marine-blue">
+                          {item.plan}
+                        </h1>
+                        <h2 className="text-neutral-cool-gray">{item.price}</h2>
+                      </div>
+                    </label>
                   </div>
-                  <div>
-                  <h1 className="text-primary-marine-blue font-medium">{item.plan}</h1>
-                  <h2 className="text-neutral-cool-gray">{item.price}</h2>
+                );
+              })
+            : yearlyPlan.map((item, index) => {
+                const isChecked = item.plan === currentPlan;
+                return (
+                  <div
+                    className={`rounded-lg border-2 p-4 hover:border-primary-purplish-blue ${isChecked ? "border-primary-purplish-blue bg-primary-purplish-blue/5" : "border-neutral-light-gray bg-neutral-white"}`}
+                    key={index}
+                  >
+                    <input
+                      type="radio"
+                      className="hidden"
+                      name="plan"
+                      id={item.plan}
+                      value={item.plan}
+                      onChange={(e) => setcurrentPlan(e.target.value)}
+                    />
+                    <label
+                      className="flex w-full flex-wrap content-center gap-4"
+                      htmlFor={item.plan}
+                    >
+                      <div className="planicon">
+                        <img src={planslogo[index]} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-medium text-primary-marine-blue">
+                          {item.plan}
+                        </h1>
+                        <h2 className="text-neutral-cool-gray">{item.price}</h2>
+                        <h2 className="text-primary-marine-blue">
+                          2 months free
+                        </h2>
+                      </div>
+                    </label>
                   </div>
-                </label>
-              </div>
-            );}):yearlyPlan.map((item, index) => {
-            const isChecked = item.plan === currentPlan;
-            return (
-              <div
-                className={` rounded-lg border-2 p-4 hover:border-primary-purplish-blue ${isChecked ? "border-primary-purplish-blue bg-primary-purplish-blue/5" : "border-neutral-light-gray bg-neutral-white"}`}
-                key={index}
-              >
-                <input
-                  type="radio"
-                  className="hidden"
-                  name="plan"
-                  id={item.plan}
-                  value={item.plan}
-                  onChange={(e) => setcurrentPlan(e.target.value)}
-                />
-                <label className=" w-full flex flex-wrap content-center gap-4 " htmlFor={item.plan}>
-                  <div className="planicon">
-                    <img src={planslogo[index]} alt="logo" />
-                  </div>
-                  <div>
-                    <h1 className="text-primary-marine-blue font-medium">{item.plan}</h1>
-                    <h2 className="text-neutral-cool-gray">{item.price}</h2>
-                    <h2 className="text-primary-marine-blue">2 months free</h2>
-                  </div>
-                </label>
-              </div>
-            );
-          })
-
-            }
+                );
+              })}
         </div>
 
-        <div className="w-full bg-neutral-magnolia p-2 rounded-md flex flex-wrap content-center justify-center gap-4">
-          <h1 className={`${!isOn?"text-primary-marine-blue font-medium":"text-neutral-cool-gray"}`}>Monthly</h1>
+        <div className="flex w-full flex-wrap content-center justify-center gap-4 rounded-md bg-neutral-magnolia p-2">
+          <h1
+            className={`${!isOn ? "font-medium text-primary-marine-blue" : "text-neutral-cool-gray"}`}
+          >
+            Monthly
+          </h1>
           <div>
             <div
-                className={`flex h-5 w-10 cursor-pointer items-center rounded-full p-1 ${
+              className={`flex h-5 w-10 cursor-pointer items-center rounded-full p-1 ${
                 isOn ? "bg-primary-marine-blue" : "bg-gray-400"
-                }`}
-                onClick={toggleSwitch}
+              }`}
+              onClick={toggleSwitch}
             >
-                <div
+              <div
                 className={`h-4 w-4 transform rounded-full bg-white shadow-md duration-300 ${
-                    isOn ? "translate-x-4" : "translate-x-0"
+                  isOn ? "translate-x-4" : "translate-x-0"
                 }`}
-                />
+              />
             </div>
           </div>
-          <h1 className={`${isOn?"text-primary-marine-blue font-medium":"text-neutral-cool-gray"}`}>Yearly</h1>
+          <h1
+            className={`${isOn ? "font-medium text-primary-marine-blue" : "text-neutral-cool-gray"}`}
+          >
+            Yearly
+          </h1>
         </div>
       </div>
       <div className="absolute bottom-0 h-[12%] w-full bg-neutral-white">
-        <FooterNav previous={"/"} next={"/step2"} />
+        <FooterNav previous={"/"} next={"/step3"} />
       </div>
     </div>
   );
--- src/routes/Step3.jsx
+++ src/routes/Step3.jsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React from "react";
+import FooterNav from "../components/footerNav";
 
-export default function Step3(){
-    return(
-        <></>
-    )
+export default function Step3() {
+  const addOns = [
+    {
+      addon: "Online service",
+      desc: "Access to multiplayer games",
+      price: "+$1/mo",
+    },
+    {
+      addon: "Larger storage",
+      desc: "Extra 1TB of cloud save",
+      price: "+$2/mo",
+    },
+    {
+      addon: "Customizable profile",
+      desc: "Custom theme on your profile",
+      price: "+$2/mo",
+    },
+  ];
+  return (
+    <div className="relative h-4/5 w-full lg:h-full">
+      <div className="absolute top-[-8%] left-[50%] flex h-auto w-[92%] translate-x-[-50%] flex-col gap-4 rounded-lg bg-neutral-white p-8 drop-shadow-xl md:w-[600px] lg:static lg:w-full lg:translate-x-0 lg:gap-6 lg:p-16 lg:drop-shadow-none">
+        <h1 className="text-2xl font-bold text-primary-marine-blue lg:text-4xl">
+          Pick add-ons
+        </h1>
+        <h2 className="text-neutral-cool-gray lg:text-xl">
+          Add-ons help enhace your gaming experience
+        </h2>
+
+        <div className="w-full flex flex-col gap-2">
+          {addOns.map((item, index) => {
+            return (
+              <label
+                key={index}
+                className={`grid grid-cols-[0.5fr_2.5fr_1fr] place-items-center content-center gap-2 border rounded-md  p-4 lg:p-8`}
+                htmlFor={item.addon}
+              >
+                <div>
+                    <input type="checkbox" className="size-5" name="addon" id={item.addon} />
+                </div>
+                
+                  <div >
+                    <h1 className="text-wrap font-medium text-primary-marine-blue">
+                      {item.addon}
+                    </h1>
+                    <h2 className="text-neutral-cool-gray">{item.desc}</h2>
+                  </div>
+                   
+                  
+
+                <div>
+                <p>{item.price}</p>
+                </div>
+              </label>
+            );
+          })}
+        </div>
+      </div>
+      <div className="absolute bottom-0 h-[12%] w-full bg-neutral-white">
+        <FooterNav previous={"/step2"} next={"/step4"} />
+      </div>
+    </div>
+  );
 }
